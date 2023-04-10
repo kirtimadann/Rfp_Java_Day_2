9ef01f5a0d00a44fd06a933c7abd18b6a928665b
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 17:17:43 +0530

    Number to string

---
+++ src/com/bl/day2/NumberInWord.java
@@ -0,0 +1,56 @@
+package com.bl.day2;
+
+public class NumberInWord {
+
+    static int var = 7;
+    static String N;
+
+    public static void main(String[] args) {
+
+
+        if (var == 0){
+
+            System.out.println("Zero");
+        } else if (var == 1) {
+
+            System.out.println("one");
+
+        } else if (var == 2) {
+
+            System.out.println("Two");
+
+        }else if (var == 3){
+            System.out.println("Thiree");
+
+        } else if (var == 4) {
+
+            System.out.println("four");
+
+        } else if (var == 5) {
+
+            System.out.println("five");
+
+        } else if (var == 6) {
+            System.out.println("six");
+
+        } else if (var == 7) {
+            System.out.println("Seven");
+
+        } else if (var == 8) {
+
+            System.out.println("eight");
+
+        } else if (var == 9) {
+
+            System.out.println("nine");
+
+        }
+        else {
+            System.out.println("print valid number ");
+        }
+
+
+
+    }
+
+}
